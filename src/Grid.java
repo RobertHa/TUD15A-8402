bd3ce0afc573a25f3069c80c7221f14a3125d6d9
mahdisellami <mahdisellami@wlan-145-94-191-11.wlan.tudelft.nl>
2017-03-14 13:10:38 +0100

    This is a working version. You can look at this revision.

--- src/Grid.java
+++ src/Grid.java
@@ -4,48 +4,48 @@ import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+public class Grid extends Canvas {
 
-public class Grid extends Canvas{
+	gameLogic M;
+	int width, height;
 
-	      gameLogic M;
-		  int width, height;
+	Grid(int w, int h, gameLogic M) {
+		setSize(width = w, height = h);
+		this.M = M;
+	}
 
-		  Grid(int w, int h,  gameLogic M) {
-		    setSize(width = w, height = h);
-		    this.M = M;
-		  }
-
-		  public void paint(Graphics g) {
-		    width = getSize().width;
-		    height = getSize().height;
-		    if(!M.gameOver){
+	public void paint(Graphics g) {
+		width = getSize().width;
+		height = getSize().height;
+		if (!M.gameOver) {
 			int rows = M.sizeMatrix;
-			  int cols = M.sizeMatrix;
-		    // draw the rows
-		    int rowHt = height / (rows);
-		    for (int i = 0; i < rows; i++)
-		      g.drawLine(0, i * rowHt, width, i * rowHt);
+			int cols = M.sizeMatrix;
+			// draw the rows
+			int rowHt = height / (rows);
+			for (int i = 0; i < rows; i++)
+				g.drawLine(0, i * rowHt, width, i * rowHt);
 
-		    // draw the columns
-		    int rowWid = width / (cols);
-		    for (int i = 0; i < cols; i++)
-		      g.drawLine(i * rowWid, 0, i * rowWid, height);
-		    
-		    // draw the numbers
-		    for (int i=0; i< rows; i++){
-		    	for (int j=0; j< cols; j++){
-		    		if (M.matrix[i][j] != 0)
-		    		g.drawString(Integer.toString(M.matrix[i][j]), (j+ 1) * rowWid - (rowWid / 2), (i+1) * rowHt - (rowHt /2));
-		    	}
-		    }
-		    } else {
-		    	g.drawString("Game Over!", width/2 - 40, height/2 - 10);
-		    	g.drawString("Your Score: " + M.score, width/2 - 45, height/2 + 10);
-		    }
-		  }
-		  
-		  public void update(){
-			  
-		  }
-		
+			// draw the columns
+			int rowWid = width / (cols);
+			for (int i = 0; i < cols; i++)
+				g.drawLine(i * rowWid, 0, i * rowWid, height);
+
+			// draw the numbers
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					if (M.matrix[i][j] != 0)
+						g.drawString(Integer.toString(M.matrix[i][j]), (j + 1) * rowWid - (rowWid / 2),
+								(i + 1) * rowHt - (rowHt / 2));
+				}
+			}
+		} else {
+			g.drawString("Game Over!", width / 2 - 40, height / 2 - 10);
+			g.drawString("Your Score: " + M.score, width / 2 - 45, height / 2 + 10);
+		}
+	}
+
+	public void update() {
+
+	}
+
 }
--- src/gameLogic.java
+++ src/gameLogic.java
@@ -1,222 +1,208 @@
 import java.util.Arrays;
 
-
 public class gameLogic {
-	int sizeMatrix=4;//size
-	int[][] matrix;//to record the value of each block
-	boolean gameStart;//flag in game
-	boolean gameOver;//flag in game
-	int score;//record the score
-	int step;//for testing
-	enum direction {left,right,up,down};//4 directions
-	
-	//construction
-	gameLogic(int size){
-		gameStart=true;
-		gameOver=false;
-		step=1;
-		sizeMatrix=size;
-		matrix=new int[size][size];
-		for (int i=0;i<size;i++){
-			for (int j=0;j<size;j++)
-				matrix[i][j]=0;
+	int sizeMatrix = 4;// size
+	int[][] matrix;// to record the value of each block
+	boolean gameStart;// flag in game
+	boolean gameOver;// flag in game
+	int score;// record the score
+	int step;// for testing
+
+	enum direction {
+		left, right, up, down
+	};// 4 directions
+
+	// construction
+	gameLogic(int size) {
+		gameStart = true;
+		gameOver = false;
+		step = 1;
+		sizeMatrix = size;
+		matrix = new int[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++)
+				matrix[i][j] = 0;
 		}
-		matrix[0][0]=matrix[0][1]=2;
-		score=0;
-		
-	}
-	
-	void setMatrix(int[][] m){
-		matrix=m.clone();
-	}
-	
-	
-	//will be used for simplifying swipe 
-	//trans turns up/down to left/right
-	//l2r turns from right to left
-	public void trans(){
-		int[][] M=new int[sizeMatrix][sizeMatrix];
-		for (int i=0;i<sizeMatrix;i++)
-			for (int j=0;j<sizeMatrix;j++)
-				M[i][j]=matrix[j][i];
-		matrix=M.clone();
-	}
-	
-	public void l2r(){
-		int[][] M=new int[sizeMatrix][sizeMatrix];
-		for (int i=0;i<sizeMatrix;i++)
-			for (int j=0;j<sizeMatrix;j++)
-				M[i][j]=matrix[i][sizeMatrix-j-1];
-		matrix=M.clone();
-	}
-	
+		matrix[0][0] = matrix[0][1] = 2;
+		score = 0;
 
-	
-	//four actions
-	
+	}
 
-	
-	public boolean swipe(direction d){
-		int[][] M=new int[sizeMatrix][sizeMatrix];
-		int[] zeroNum=new int[sizeMatrix];
-		int i,j,k;
-		boolean changed=false;
+	void setMatrix(int[][] m) {
+		matrix = m.clone();
+	}
 
-		
-		if (d==direction.left){
-			for (i=0;i<sizeMatrix;i++){
-				
-				zeroNum[i]=sizeMatrix;//record the number of zeros in each line
-		
-				k=0;
-				for (j=0;j<sizeMatrix;j++){
-					if (matrix[i][j]!=0){					
-						if (M[i][k]==0){
-							M[i][k]=matrix[i][j];
+	// will be used for simplifying swipe
+	// trans turns up/down to left/right
+	// l2r turns from right to left
+	public void trans() {
+		int[][] M = new int[sizeMatrix][sizeMatrix];
+		for (int i = 0; i < sizeMatrix; i++)
+			for (int j = 0; j < sizeMatrix; j++)
+				M[i][j] = matrix[j][i];
+		matrix = M.clone();
+	}
+
+	public void l2r() {
+		int[][] M = new int[sizeMatrix][sizeMatrix];
+		for (int i = 0; i < sizeMatrix; i++)
+			for (int j = 0; j < sizeMatrix; j++)
+				M[i][j] = matrix[i][sizeMatrix - j - 1];
+		matrix = M.clone();
+	}
+
+	// four actions
+
+	public boolean swipe(direction d) {
+		int[][] M = new int[sizeMatrix][sizeMatrix];
+		int[] zeroNum = new int[sizeMatrix];
+		int i, j, k;
+		boolean changed = false;
+
+		if (d == direction.left) {
+			for (i = 0; i < sizeMatrix; i++) {
+
+				zeroNum[i] = sizeMatrix;// record the number of zeros in each
+										// line
+
+				k = 0;
+				for (j = 0; j < sizeMatrix; j++) {
+					if (matrix[i][j] != 0) {
+						if (M[i][k] == 0) {
+							M[i][k] = matrix[i][j];
+							zeroNum[i]--;
+						} else if (M[i][k] == matrix[i][j]) {
+							M[i][k] = matrix[i][j] * 2;
+							k++;
+						} else {
+							k++;
+							M[i][k] = matrix[i][j];
 							zeroNum[i]--;
 						}
-						else if (M[i][k]==matrix[i][j]){
-							M[i][k]=matrix[i][j]*2;
-							k++;
-						}
-						else{
-							k++;
-							M[i][k]=matrix[i][j];
-							zeroNum[i]--;
-						}
-					}					
+					}
 				}
 
-				//see if there is any change
-				if (!Arrays.equals(matrix[i], M[i])){
-					changed=true;
+				// see if there is any change
+				if (!Arrays.equals(matrix[i], M[i])) {
+					changed = true;
 				}
 			}
-		
-		
-			if (changed==false)
-			{
+
+			if (changed == false) {
 				System.out.print("Same!\n");
-				if (isFull()){
-					//full& not movable leads to gameover
-					if (!isMovable()){ 
-						gameOver=true;//game over
+				if (isFull()) {
+					// full& not movable leads to gameover
+					if (!isMovable()) {
+						gameOver = true;// game over
 						calcScore();
 					}
 				}
-			}
-			else
-			{
-				matrix=M.clone();
+			} else {
+				matrix = M.clone();
 				generateNewBlock(zeroNum);// all direction here is left
 			}
 		}
-		
-		
-		if (d==direction.right){
+
+		if (d == direction.right) {
 			l2r();
-			changed=swipe(direction.left);
+			changed = swipe(direction.left);
 			l2r();
 		}
-		
-		if (d==direction.up){
+
+		if (d == direction.up) {
 			trans();
-			changed=swipe(direction.left);
-			trans();
-		}
-		if (d==direction.down){
-			trans();
-			l2r();
-			changed=swipe(direction.left);
-			l2r();
+			changed = swipe(direction.left);
 			trans();
 		}
-		
+		if (d == direction.down) {
+			trans();
+			l2r();
+			changed = swipe(direction.left);
+			l2r();
+			trans();
+		}
+
 		return changed;
-		
+
 	}
 
-	//calculates highest tile when you are gameover
-	void calcScore(){
-		for (int i = 0; i <this.sizeMatrix ; i++) {
+	// calculates highest tile when you are gameover
+	void calcScore() {
+		for (int i = 0; i < this.sizeMatrix; i++) {
 			for (int j = 0; j < this.sizeMatrix; j++) {
-				score = Math.max(score,this.matrix[i][j]);
+				score = Math.max(score, this.matrix[i][j]);
 			}
 		}
 	}
 
-	//see if full
-	public boolean isFull(){
-		for (int i=0;i<sizeMatrix;i++){
-			for (int j=0;j<sizeMatrix;j++)
-				if (matrix[i][j]==0){
+	// see if full
+	public boolean isFull() {
+		for (int i = 0; i < sizeMatrix; i++) {
+			for (int j = 0; j < sizeMatrix; j++)
+				if (matrix[i][j] == 0) {
 					return false;
 				}
 		}
 		return true;
 	}
-	
 
-	//see if movable
-	public boolean isMovable(){
-		for (int i=0;i<sizeMatrix;i++){
-			for (int j=0;j<sizeMatrix-1;j++)
-				if (matrix[i][j]==matrix[i][j+1] || matrix[j][i]==matrix[j+1][i]){
+	// see if movable
+	public boolean isMovable() {
+		for (int i = 0; i < sizeMatrix; i++) {
+			for (int j = 0; j < sizeMatrix - 1; j++)
+				if (matrix[i][j] == matrix[i][j + 1] || matrix[j][i] == matrix[j + 1][i]) {
 					return true;
 				}
 		}
 		return false;
 	}
 
-	public void generateNewBlock(int[] znum){
-		int numZero=0;
+	public void generateNewBlock(int[] znum) {
+		int numZero = 0;
 		int i;
-		//first count the number
-		for (i=0;i<sizeMatrix;i++){
-			numZero=numZero+znum[i];
+		// first count the number
+		for (i = 0; i < sizeMatrix; i++) {
+			numZero = numZero + znum[i];
 		}
-		int newPos=(int)(Math.random()*numZero);//decide the position
-		i=0;
-		while (newPos>=znum[i]){
-			newPos=newPos-znum[i++];
+		int newPos = (int) (Math.random() * numZero);// decide the position
+		i = 0;
+		while (newPos >= znum[i]) {
+			newPos = newPos - znum[i++];
 		}
-		//generate a new block
-		//if (Math.random()>0.3){
-			matrix[i][sizeMatrix-newPos-1]=2;//2 or 4
-		//}
-		//else{
-		//	matrix[i][sizeMatrix-newPos-1]=4;
-		//}
-					
-		
+		// generate a new block
+		// if (Math.random()>0.3){
+		matrix[i][sizeMatrix - newPos - 1] = 2;// 2 or 4
+		// }
+		// else{
+		// matrix[i][sizeMatrix-newPos-1]=4;
+		// }
+
 	}
-	
-	//restart
-	void restart(int size){
-		gameStart=true;
-		gameOver=false;
-		step=1;
-		sizeMatrix=size;
-		matrix=new int[size][size];
-		for (int i=0;i<size;i++){
-			for (int j=0;j<size;j++)
-				matrix[i][j]=0;
+
+	// restart
+	void restart(int size) {
+		gameStart = true;
+		gameOver = false;
+		step = 1;
+		sizeMatrix = size;
+		matrix = new int[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++)
+				matrix[i][j] = 0;
 		}
-		matrix[0][0]=matrix[0][1]=2;
-		score=0;	
+		matrix[0][0] = matrix[0][1] = 2;
+		score = 0;
 	}
-	
-	
-	
-	//for testing
-	public void show(){
-		System.out.format("Step%d:\n\n",step);
-		for (int i=0;i<sizeMatrix;i++){
-			for (int j=0;j<sizeMatrix;j++)
-				System.out.format("%d\t",matrix[i][j]);
+
+	// for testing
+	public void show() {
+		System.out.format("Step%d:\n\n", step);
+		for (int i = 0; i < sizeMatrix; i++) {
+			for (int j = 0; j < sizeMatrix; j++)
+				System.out.format("%d\t", matrix[i][j]);
 			System.out.println('\n');
 		}
-		step=step+1;
+		step = step + 1;
 		System.out.println('\n');
 	}
 
