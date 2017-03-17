84ace143d3541a65523ddd5cbe58f81e1fecd48a
mahdisellami <mahdisellami@wlan-145-94-191-11.wlan.tudelft.nl>
2017-03-17 10:38:28 +0100

    Improve the Grid and Display

--- src/Grid.java
+++ src/Grid.java
@@ -18,7 +18,9 @@ public class Grid extends Canvas {
 
 	public void paint(Graphics g) {
 
-		Color[] c={Color.cyan,Color.orange,Color.lightGray,Color.magenta};
+		Color[] c={Color.white,Color.white, new Color(255,255,170), new Color(255,255,128), new Color(255,255,85)
+				, new Color(255,255,43), new Color(255,255,0), new Color(213,213,0), new Color(170,170,0)
+				, new Color(128,128,0), new Color(85,85,0), new Color(43,43,0)};
 		width = getSize().width - 200;
 		height = getSize().height;
 
@@ -36,16 +38,18 @@ public class Grid extends Canvas {
 				for (int j = 0; j < cols; j++) {
 					if (game.board.board[i][j] != 0)
 					{
-						int n=(int)(Math.log(game.board.board[i][j])/Math.log(2));//get the number in form of 1-n
-						g.setColor(c[Math.floorMod(n, 4)]);
+						int value = game.board.board[i][j];
+						int n=(int)(Math.log(value)/Math.log(2));//get the number in form of 1-n
+						g.setColor(c[Math.floorMod(n, c.length)]);
 						g.fillRect(rowWid*j, rowHt*i, rowWid, rowHt);
-						g.setColor(Color.black);
+						g.setColor(getTextColor(value));
 						g.setFont(new Font("Default",Font.PLAIN,sizeFont));;
-						g.drawString(Integer.toString(game.board.board[i][j]), (j + 1) * rowWid - (rowWid / 2),
+						g.drawString(Integer.toString(value), (j + 1) * rowWid - (rowWid / 2) - getTextShifting(value),
 								(i + 1) * rowHt - (rowHt / 2));
 					}
 				}
 			}
+			g.setColor(Color.white);
 			for (int i = 0; i < rows; i++)
 				g.drawLine(0, i * rowHt, width, i * rowHt);
 			for (int i = 0; i <= cols; i++)
@@ -70,8 +74,17 @@ public class Grid extends Canvas {
 		}
 	}
 
-	public void update() {
-
+	private Color getTextColor(int value){
+		return (value >= 256) ? Color.white : Color.black; 
+	}
+	
+	private int getTextShifting(int value){
+		if (value < 10)
+			return 0;
+		else if (value < 100)
+			return 10;
+		else 
+			return 20;
 	}
 
 }
